b7da8ec7b445a5d1b6a98ab06ee1d8f105f7e04f
Golovadvauxa <parenek.serega@gmail.com>
2021-12-16 17:15:49 +0300

    exercise '04_fractal' in progress

--- 05_snowfall.py
+++ 05_snowfall.py
@@ -2,32 +2,51 @@
 
 import simple_draw as sd
 
+sd.resolution = (1200, 600)
+
 
 # На основе кода из практической части реализовать снегопад:
 # - создать списки данных для отрисовки N снежинок
 # - нарисовать падение этих N снежинок
 # - создать список рандомных длинн лучей снежинок (от 10 до 100) и пусть все снежинки будут разные
 
-N = 20
-
-# Пригодятся функции
-# sd.get_point()
-# sd.snowflake()
-# sd.sleep()
-# sd.random_number()
-# sd.user_want_exit()
-
-# TODO здесь ваш код
-while True:
-    sd.clear_screen()
-    pass
-    pass
-    pass
-    sd.sleep(0.1)
-    if sd.user_want_exit():
-        break
-
-sd.pause()
+# N = 20
+# coord_list = []
+# length_list = []
+# for i in range(N):
+#     fill = [sd.random_number(0, 1200), 600]
+#     coord_list.append(fill)
+#     fill = sd.random_number(10,100)
+#     length_list.append(fill)
+# #print(coord_list)
+# # Пригодятся функции
+# # sd.get_point()
+# # sd.snowflake()
+# # sd.sleep()
+# # sd.random_number()
+# # sd.user_want_exit()
+#
+#
+# while True:
+#     sd.clear_screen()
+#     for i in range(N):
+#         x, y = coord_list[i]
+#         if x<0:
+#             x=sd.random_number(0,1200)
+#         if y<0:
+#             y=600
+#
+#         point = sd.get_point(x, y)
+#         sd.snowflake(center=point, length=length_list[i])
+#         x += sd.random_number(-10, 10)
+#         y -= sd.random_number(0, 20)
+#         coord_list[i] = x, y
+#
+#     sd.sleep(0.1)
+#     if sd.user_want_exit():
+#         break
+#
+# sd.pause()
 
 # подсказка! для ускорения отрисовки можно
 #  - убрать clear_screen()
@@ -43,5 +62,40 @@ sd.pause()
 # - сделать сугоб внизу экрана - если снежинка долетает до низа, оставлять её там,
 #   и добавлять новую снежинку
 # Результат решения см https://youtu.be/XBx0JtxHiLg
+N = 20
+coord_list = []
+length_list = []
+for i in range(N):
+    fill = [sd.random_number(0, 1200), 700]
+    coord_list.append(fill)
+    fill = sd.random_number(10, 100)
+    length_list.append(fill)
+while True:
 
+    sd.clear_screen()
+    # for j in range(N):
+    #     x0, y0 = coord_list[j]
+    #     if y0 < 10:
+    #         point0 = sd.get_point(x0, 50)
+    #         sd.snowflake(center=point0, length=length_list[j])
 
+    for i in range(N):
+
+        x, y = coord_list[i]
+        if x < 0:
+            x = sd.random_number(0, 1200)
+        if y <= 0:
+            y=700
+        point = sd.get_point(x, y)
+
+        sd.snowflake(center=point, length=length_list[i])
+        x += sd.random_number(-10, 10)
+        y -= sd.random_number(0, 50)
+        coord_list[i] = x, y
+    if y<0:
+        continue
+    sd.sleep(0.1)
+    if sd.user_want_exit():
+        break
+
+sd.pause()
